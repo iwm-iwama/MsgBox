d098dc6106abb1cddbe9e37d5dc7f005ed278d0b
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2020-07-30 12:34:48 +0900

    Add files via upload

--- sample.bat
+++ sample.bat
@@ -4,7 +4,7 @@ cls
 iwm_MsgBox.exe
 echo %errorlevel%
 
-iwm_MsgBox.exe -title="タイトル" -text="あああ\\ああ^\tいい\いいい^\n^\n^\nううううう" -button=1,1
+iwm_MsgBox.exe -title="タイトル" -text="あああ\\ああ^\tいい\いいい^\n^\n^\nううううう" -size=400,160 -textsize=20 -button=1,1 -button2="Yes","No","Cancel"
 echo %errorlevel%
 
 echo.
