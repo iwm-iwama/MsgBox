c067e8ab2dda26cc52cbe6188094ddfdde11f898
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2021-06-18 08:41:46 +0900

    Add files via upload

--- sample.bat
+++ sample.bat
@@ -1,9 +1,30 @@
 @echo off
 cls
 
+::---------
+:: ヘルプ
+::---------
 for /f "usebackq delims=" %%s in (`"iwm_MsgBox.exe"`) do echo %%s
 
-for /f "usebackq delims=" %%s in (`"iwm_MsgBox.exe -size=250,160 -title="タイトル" -text="あいうえお\\nかき\\tくけこ" -textsize=10 -checkbox="上記内容を承諾します。" -button=1,1 -buttontext="はい","いいえ","閉じる""`) do echo %%s
+::-------------
+:: １行で記述
+::-------------
+for /f "usebackq delims=" %%s in (`"iwm_MsgBox.exe -size=250,160 -title="１行で記述" -text="オプションを\\n１行で記述した例" -textsize=10 -checkbox="上記内容を承諾します。" -button=1,1 -buttontext="はい","いいえ","閉じる""`) do echo %%s
+
+::---------------------
+:: 変数利用による記述
+::---------------------
+set SIZE=250,160
+set TITLE=変数利用による記述
+set TEXT= ^
+オプションを\\n ^
+変数を利用して記述した例
+set TEXTSIZE=10
+set CHECKBOX=上記内容を承諾します。
+set BUTTON=1,1
+set BUTTONTEXT="はい","いいえ","閉じる"
+
+for /f "usebackq delims=" %%s in (`"iwm_MsgBox.exe -size=%SIZE% -title="%TITLE%" -text="%TEXT%" -textsize=%TEXTSIZE% -checkbox="%CHECKBOX%" -button=%BUTTON% -buttontext=%BUTTONTEXT%"`) do echo %%s
 
 echo.
 pause
